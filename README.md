677471f5cccf9914d084c1f3d75c66583c9b7b61
lucaso60 <lucasoAlt@outlook.com>
2021-08-15 13:03:21 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+
+# DiscordChannelSpammer
+
+---
+___
+
+# LICENSE
+MIT License
+
+Copyright (c) 2021 lucaso60
+
+Copyright (c) 2015-present Rapptz
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVUSER_IDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+_____
+
+This code is using a library called discord.py by Copyright (c) 2015-present Rapptz, his project is also licensed under the MIT License. 
+Rapptz repository is in https://github.com/Rapptz/discord.py.
+_______________
+
+# How to use
+- Download Python from python.org
+- Install discord.py using pip: pip `install discord.py` / `pip3 install discord.py`
+- Click and run the luancher.py.
+- Remember to have a your bot TOKEN from https://discord.com/developers/applications
+- Also remember that you have to get the channel USER_ID by turning on developer mode in discord in settings under advanced.
+----
+# Contact
+You can contact me at lucasoAlt@outlook.com.
---
+++ launcher.py
@@ -0,0 +1,57 @@
+"""
+MIT License
+
+Copyright (c) 2021 lucaso60
+Copyright (c) 2015-present Rapptz
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVUSER_IDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+import discord
+from discord.ext import commands
+from discord import *
+from datetime import datetime
+from time import sleep
+
+from extensions import on_start_screen
+
+def time_now():
+    time = datetime.now()
+    current_time = time.strftime("%y-%m-%d %H:%M:%S")
+    now = current_time + " >"
+    return now
+
+TOKEN = input(f"{time_now()} Please input your bot token: ")
+
+
+bot = commands.Bot(command_prefix=".")
+
+@bot.event
+async def on_ready():
+    print(f"{time_now()} Logged in as {bot.user}")
+    USER_ID = input(f"{time_now()} Please input USER ID: ")
+    MESSAGE = input(f"{time_now()} Please input the spam message: ")
+    user =  await bot.fetch_user(USER_ID)
+    while True:
+        await user.send(MESSAGE)
+        print(f"{time_now()} Spammed {user} with {MESSAGE}") 
+        sleep(0.8)
+
+bot.run(TOKEN)
+
+
